c20d628958609f37ccd0d400ba76f47b42e4289f
IT24102228 <it24102228@my.sliit.lk>
2026-04-29 10:19:46 +0530

    sd

--- mern-item-manager/frontend/src/api.js
+++ mern-item-manager/frontend/src/api.js
@@ -5,5 +5,4 @@ baseURL: import.meta.env.VITE_API_URL,
 export const getItems = () => API.get('/items');
 export const createItem = (data) => API.post('/items', data);
 export const deleteItem = (id) => API.delete(`/items/${id}`);
-export const updateItem = (id, data) => API.put(`/items/${id}
-`, data);
+export const updateItem = (id, data) => API.put(`/items/${id}`, data);
